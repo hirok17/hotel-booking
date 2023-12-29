361e4b0ae45ab1b25d59fa9863d843314c4917d1
hirok17 <hirokray17@gmail.com>
2023-12-29 23:10:58 +0600

    project setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Container from "../../components/Shared/Container"
+
+
+const Home = () => {
+  return (
+    <div>
+      <Container>
+      <h1>Welcome to StayVista</h1>
+      </Container>
+      
+    </div>
+  )
+}
+
+export default Home
